9017ff4851b53a3cc145fa33422da8b99e832d26
Peter Stephenson <PeterStephenson@users.noreply.github.com>
2020-06-04 21:42:21 +0100

    Add files via upload

---
+++ ubuntu-setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Ubuntu setup
+
+# Allow HTTPS packages
+sudo apt-get update
+
+$ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+sudo apt-get update
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Beyond Compare
+wget https://www.scootersoftware.com/bcompare-4.3.4.24657_amd64.deb
+sudo apt-get update
+sudo apt-get install gdebi-core
+sudo gdebi bcompare-4.3.4.24657_amd64.deb
+
+# VS code
+sudo snap install --classic code
+
+# Git
+sudo apt install git
+git config --global user.name "Peter Stephenson"
+git config --global user.email "github@peter-stephenson.co.uk"
+git config --global alias.recent "log -10 --oneline"
+
+git config --global merge.tool bc4
+git config --global mergetool.bc4.trustExitCode true
+git config --global diff.tool bc4
+
+git config --global core.editor "code --wait"
+
+# Rider
+sudo snap install rider --classic
+
+# .Net Core
+wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install dotnet-sdk-3.1
+sudo apt-get install aspnetcore-runtime-3.1
+
+# JS
+sudo apt install nodejs
+sudo apt install npm
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
